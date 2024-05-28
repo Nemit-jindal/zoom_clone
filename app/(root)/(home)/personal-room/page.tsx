20e7f6f74fa28eb7d67c9e15db26d259e55cd7b3
Nemit-jindal <nemitjindal5016@gmail.com>
2024-05-28 23:17:45 +0530

    Updated Link

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -17,7 +17,7 @@ const Table=({title,description}:{title:string;description:string;})=>(
 const PersonalRoom = () => {
   const {user}=useUser();
   const meetingId=user?.id;
-  const meetingLink=`${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}? personal=true`
+  const meetingLink=`https://${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}? personal=true`
   const {toast}=useToast();
   const client=useStreamVideoClient();
   const router=useRouter()
